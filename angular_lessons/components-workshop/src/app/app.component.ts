5a350138ebf51b4f37905aa6227c6ce96bd4c6b7
Dimitar Petrov <dimitar.petrov@me.com>
2024-11-18 22:08:00 +0200

    stage

--- angular_lessons/components-workshop/src/app/app.component.ts
+++ angular_lessons/components-workshop/src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./core/header/header.component";
 import { FooterComponent } from "./core/footer/footer.component";
 import { MainComponent } from './main/main.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, FooterComponent, MainComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, MainComponent, HttpClientModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
